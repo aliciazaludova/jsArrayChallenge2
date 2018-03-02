e5d47b68eb1dcee217649f45e28505dd618cce7b
Alicia Zaludova <aliciazaludova@gmail.com>
2018-03-02 09:57:45 -0600

    setup

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log("here is a string");
+
+// Write some code that takes an array of numbers and outputs an array that changes any 3s to 7s and any 7s to 3s.
+
+// var challengeTwoInputA = [1,2,3,4,5,6,7,8,9];  //[1,2,7,4,5,6,3,8,9];
